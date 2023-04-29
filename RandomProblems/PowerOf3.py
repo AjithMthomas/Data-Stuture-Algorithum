e0cd38205a9b751649970af56cbbe90cda428b2a
AjithMthomas <ajithmt46958@gmail.com>
2023-04-29 10:16:27 +0530

    power of numbers

---
+++ RandomProblems/PowerOf3.py
@@ -0,0 +1,13 @@
+n=5
+array=[]
+result = 1
+for i in range(n):
+    array.append(result)
+    result*=3
+print(array)
+
+# n=5
+# array=[]
+# for i in range(n):
+#     array.append(3**i)
+# print(array)
