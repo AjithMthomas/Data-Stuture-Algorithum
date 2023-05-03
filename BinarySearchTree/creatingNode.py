1bdf71daf19e72eff36edec302639229eacb8363
AjithMthomas <ajithmt46958@gmail.com>
2023-05-03 11:41:48 +0530

    binary search tree

---
+++ BinarySearchTree/creatingNode.py
@@ -0,0 +1,6 @@
+class BST:
+    def __init__(self,key) :
+        self.key = key
+        self.lchild = None
+        self.rchild = None
+# this will create a node with a key and its childs as None
---
+++ BinarySearchTree/insertionInBST.py
@@ -0,0 +1,19 @@
+class BST:
+    def __init__(self,key) :
+        self.key = key
+        self.lchild = None
+        self.rchild = None
+    def insertion(self,data):
+        if self.key is None:
+            self.key = data
+            return
+        if self.key > data:
+             if self.lchild:
+                 self.lchild.insertion(data)
+             else:
+                 self.lchild = BST(data)
+        else:
+            if self.rchild:
+                self.rchild.insertion(data)
+            else:
+                self.rchild = BST(data)
