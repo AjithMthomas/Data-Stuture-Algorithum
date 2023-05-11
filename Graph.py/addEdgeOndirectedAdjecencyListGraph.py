3a40fa924be412592298c6f200dcba980947214c
AjithMthomas <ajithmt46958@gmail.com>
2023-05-11 17:09:45 +0530

    depth first search

--- Graph.py/addEdgeOndirectedAdjecencyListGraph.py
+++ Graph.py/addEdgeOndirectedAdjecencyListGraph.py
@@ -1,4 +1,5 @@
 graph={}
+visited = set()
 
 def add_node(v):
     if v in graph:
@@ -13,8 +14,18 @@ def add_edge(v1,v2,cost):
     else:
         list1=[v2,cost]
         graph[v1].append(list1)
+def DFS(node,visited,graph):
+    if node not in graph:
+        print("node is not in graph")
+        return
+    if node not in visited:
+        print(node)
+        visited.append(node)
+        for i in graph[node]:
+            DFS(i,visited,graph)
 add_node('a')
 add_node('b')
 add_node('c')
 add_edge('a','c',10)
+graph = DFS()
 print(graph)
