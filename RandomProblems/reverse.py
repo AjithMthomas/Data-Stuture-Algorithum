9178b28328fe0e4687d4cdcef8ab10a4f418cb55
AjithMthomas <ajithmt46958@gmail.com>
2023-06-21 12:46:04 +0530

    reverse

---
+++ RandomProblems/reverse.py
@@ -0,0 +1,4 @@
+n = [1,4,5]
+
+for i in range(len(n)):
+    print(i)
---
+++ RandomProblems/reversed string.py
@@ -0,0 +1,10 @@
+
+def reversed(a):
+    result =""
+    for i in a:
+        result = i + result
+    return result
+s = 'aji'
+b = reversed(s)
+print(b)
+
